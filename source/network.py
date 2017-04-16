a708e86a6a03a437360d80c7e2e7cc8ea2e0a39e
ccit <1414970078@qq.com>
2017-04-16 15:39:52 +0800

    添加了部分config选项，使用全局json来调整参数。

--- source/network.py
+++ source/network.py
@@ -12,20 +12,31 @@ from keras.models import Sequential
 from keras.layers import Dense,Activation,Embedding
 from keras.layers import LSTM
 from util import one_hot
+import os
+import json
 
 #需要修改
-path = "/home/chenzewei/GraduationProject/tmp/"
+root = os.path.abspath("..")
+f = open(root+"/config.json","r")
+config = json.load(f)
+maxlen = config['length']
+max_features =config['max_features']
+batch_size = config['batch_size']
+wordcnt = config['nb_words']
+f.close()
 
-max_features = 120000
-maxlen = 10  # cut texts after this number of words (among top max_features most common words)
-batch_size = 32
+path = root+"/tmp/"
+
+#max_features = 120000
+#maxlen = 10  # cut texts after this number of words (among top max_features most common words)
+#batch_size = 32
 
 print('Loading data...')
 #导入数据
 xfile = path+"classes_seg.txt"
 yfile = path+"classesY.txt"
 
-X_train = one_hot.one_hot(xfile,n=114120,maxlen=maxlen,split = " ")
+X_train = one_hot.one_hot(xfile,n=wordcnt,maxlen=maxlen,split = " ")
 y_train = one_hot.replace(yfile)
 
 files = ["test","test2","test3","test4","test5"]
@@ -37,12 +48,8 @@ y_test = []
 for i in range(0, len(files)):
 	x_t.append(path+files[i]+"_seg.txt")
 	y_t.append(path+files[i]+"Y.txt")
-	x_test.append(one_hot.one_hot(x_t[i],n=114120,maxlen=maxlen,split = " "))
+	x_test.append(one_hot.one_hot(x_t[i],n=wordcnt,maxlen=maxlen,split = " "))
 	y_test.append(one_hot.replace(y_t[i]))
-
-
-# X_test = one_hot.one_hot(xtest,n=32000,maxlen=maxlen,split = " ")
-# y_test = one_hot.one_hot(ytest,n=7,maxlen=maxlen,split = " ")
 
 
 print(len(X_train)," ",len(y_train))
@@ -55,7 +62,7 @@ X_train = sequence.pad_sequences(X_train,padding='pre',truncating='pre',maxlen=m
 for i in range(0, len(files)):
 	x_test[i] = sequence.pad_sequences(x_test[i],padding='pre',truncating='pre',maxlen=maxlen)
 	print('X_test ',i,' shape:', x_test[i].shape)
-# X_test = sequence.pad_sequences(X_test,padding='pre',truncating='pre',maxlen=maxlen)
+
 print('X_train shape:', X_train.shape)
 
 
@@ -87,5 +94,6 @@ json_string = model.to_json()
 json = open(path+'json.txt','w')
 print(json_string,file=json)
 
-predict = model.predict_classes("../tmp/test2_seg.txt", batch_size=32, verbose=1)
+prend = one_hot.one_hot(path+"test2_seg.txt",n=wordcnt,maxlen=maxlen,split = " ")
+predict = model.predict_classes(prend, batch_size=32, verbose=1)
 print(predict)
